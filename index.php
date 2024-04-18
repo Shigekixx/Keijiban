17e514be21c3eda2a5344d0f473ffec091bf6a3f
Shigeki Shizawa <s.shizawa@so-ra.co.jp>
2024-04-18 11:42:42 +0900

    一旦エラーと空白含めてやってみた、これから調整

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php
     //サーバーのMETHODがPOSTのときの処理だよっていう目印
     if ($_SERVER["REQUEST_METHOD"]==="POST") {
+
         $title = $_POST ["title"];  //タイトルのこと
         $news = $_POST ["news"];  //記事のこと
         $str = $title.$news;    //それぞれをまとめたもの
-        //主キーを保存する
 
-        //取り出した内容をファイルに保存する
-        $file = fopen("news.txt", "a"); //ファイルを開く
-        fwrite( $file, $str ."\n");     //ファイルに書き込む
-        fclose( $file );                //ファイルを閉じる
 
-        //リロードで再度送信はこれで解消
-        header("location:http://localhost:8888/");
-        exit;
+        // 制限値 タイトルを30文字以下にしたい
+        $limit = 30;
+        // エラーメッセージ用変数
+        $errMsg = '';
+        // 入力された文字列の長さを取得する
+        $titleLength = strlen($title);
 
-    } else { 
+
+        //どっちも中身が埋まっていた場合
+        if (isset($str)){
+
+            //主キーを保存する??
+
+            //取り出した内容をファイルに保存する
+            $file = fopen("news.txt", "a"); //ファイルを開く
+            fwrite( $file, $str ."\n");     //ファイルに書き込む
+            fclose( $file );                //ファイルを閉じる
+
+            //リロードで再度送信はこれで解消
+            header("location:http://localhost:8888/");
+            exit;
+
+        //タイトルが長すぎる場合
+
+        }else  if ($limit < $titleLength ) {
+            $errMsg = 'タイトルは30文字以下で表示してください';
+        }      
+
+        //空欄が存在する場合
+        else {                     
+            $errMsg = '空欄の項目があります';
+        }  
+
+    }else { 
         //タイトル・記事出力
         //読み込みたいファイルのpathを記述し、変数に代入
         $filename = 'news.txt';
@@ -66,4 +91,3 @@
         </form>
     </body>
 </html>
-
